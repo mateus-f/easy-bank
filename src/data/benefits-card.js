1778fb71d348c367b2190878a0bef76fd97294f6
Mateus Ferreira <mateusjunior803@gmail.com>
2025-06-15 22:23:39 -0300

    :sparkles: feat: Add data file for Benefits component

---
+++ src/data/benefits-card.js
@@ -0,0 +1,27 @@
+import Online from "@assets/icons/icon-online.svg";
+import Budgeting from "@assets/icons/icon-budgeting.svg";
+import Onboarding from "@assets/icons/icon-onboarding.svg";
+import Api from "@assets/icons/icon-api.svg";
+
+export const cards = [
+  {
+    image: Online,
+    title: "Online Banking",
+    content: "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+  },
+  {
+    image: Budgeting,
+    title: "Simple Budgeting",
+    content: "See exactly where your money goes each month. Receive notifications when you're close to hitting your limits.",
+  },
+  {
+    image: Onboarding,
+    title: "Fast Onboarding",
+    content: "We don't do branches. Open your account in minutes online and start taking control of your finances right away.",
+  },
+  {
+    image: Api,
+    title: "Open API",
+    content: "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
+  },
+];
